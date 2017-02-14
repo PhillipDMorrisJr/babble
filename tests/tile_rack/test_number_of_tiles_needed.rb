1f6f8c0616c2055cc7c0042c4b22a6d922a1eaea
student <student@gowolves>
2017-02-14 14:38:36 -0500

    Done

--- tests/tile_rack/test_number_of_tiles_needed.rb
+++ tests/tile_rack/test_number_of_tiles_needed.rb
@@ -1,20 +1,38 @@
 require_relative "../../tile_rack.rb"
+require_relative "../../tile_group.rb"
 require 'minitest/autorun'
 class TileRack::TestNumberOfTilesNeeded < MiniTest::Test
 
 	def setup
+		@rack = TileRack.new
 	end
 
 	def test_empty_tile_rack_should_need_max_tiles
+		assert_equal 7, @rack.number_of_tiles_needed
 	end
 
 	def test_tile_rack_with_one_tile_should_need_max_minus_one_tiles
+		@rack.append(:A)
+		assert_equal 6, @rack.number_of_tiles_needed
 	end
 
 	def test_tile_rack_with_several_tiles_should_need_some_tiles
+		@rack.append(:A)
+		@rack.append(:C)
+		@rack.append(:D)
+		@rack.append(:Q)
+		assert_equal 3, @rack.number_of_tiles_needed
 	end
 
 	def test_that_full_tile_rack_doesnt_need_any_tiles
+		@rack.append(:C)
+		@rack.append(:D)
+		@rack.append(:Q)
+		@rack.append(:A)
+		@rack.append(:E)
+		@rack.append(:G)
+		@rack.append(:W)
+		assert_equal 0, @rack.number_of_tiles_needed
 	end
 end
 
--- tile_rack.rb
+++ tile_rack.rb
@@ -1,13 +1,38 @@
-require "tile_group"
-
+require_relative "tile_group"
+require_relative "word.rb"
 class TileRack < TileGroup
+	Number_Of_Tiles_Needed_On_Rack = 7
+
+	def initialize()
+		super
+	end
 
 	def number_of_tiles_needed
+		Number_Of_Tiles_Needed_On_Rack-tiles.length
 	end
 
 	def has_tiles_for?(text)
+		text.downcase.each_char do|character|
+			if (hand.downcase.count(character) >= text.downcase.count(character))
+				return true
+			else 
+				return false
+			end
+		end
 	end
-
+	
 	def remove_word(text)
+		word = ""
+		if (!has_tiles_for?(text))
+			return	
+		end
+		text.each_char do|character|
+			if (hand.include? character)
+				word.concat(character)
+				remove(character.to_sym)
+			end
+		end
+		return word
 	end
+	
 end
